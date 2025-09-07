38344d9d6c2824955b17f3111fc8dc96d5ce3b06
lgareffi <luciagareffi@gmail.com>
2025-09-07 12:06:09 -0300

    activar front

--- js/misPublicaciones.js
+++ js/misPublicaciones.js
@@ -104,4 +104,5 @@ document.addEventListener('DOMContentLoaded', async () => {
     console.error("Error al obtener publicaciones:", error);
     container.innerHTML = `<div class="alert alert-danger">Error al conectar con el servidor.</div>`;
   }
+  
 });
